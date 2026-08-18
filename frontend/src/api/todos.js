0942d212f20d7c82a55ab2216e2e5f962ad4b4eb
lhn_nryn <dubmate2244@gmail.com>
2026-08-18 08:18:39 +0545

    remote the hard coded api link

--- frontend/src/api/todos.js
+++ frontend/src/api/todos.js
@@ -1,4 +1,4 @@
-const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5050/api";
+const API_URL = import.meta.env.VITE_API_URL;
 
 async function handleResponse(res) {
     if (!res.ok) {
